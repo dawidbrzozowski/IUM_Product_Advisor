69887b236dc02f3e84c60e311f8cef61d7638dc4
dawidbrzozowski <dawidbrzozowski98@gmail.com>
2020-03-31 18:07:12 +0200

    readme, requirements, files_io

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# IUM_Product_Advisor
+# IUM Product Recommendation
+
+### Install all needed packages by using: pip install -r requirements.txt
---
+++ utils/files_io.py
@@ -0,0 +1,14 @@
+import json
+import os
+
+
+def load_jsonl(path):
+    with open(path, 'r') as json_file:
+        return list(json_file)
+
+
+def write_json_file(filename, data):
+    _, ext = os.path.splitext(filename)
+    filename = filename if ext == ".json" else f'{filename}.json'
+    with open(filename, 'w') as out_file:
+        json.dump(data, out_file, ensure_ascii=False, indent=2)
