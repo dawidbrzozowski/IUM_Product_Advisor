3a19aaac2ee0311f9d0af6c5086f8ffa6e2cab53
dawidbrzozowski <dawidbrzozowski1998@gmail.com>
2020-04-22 23:20:49 +0200

    added session cleaning, code needs huge refactor

--- preprocessing/preprocessor.py
+++ preprocessing/preprocessor.py
@@ -1,4 +1,4 @@
-from utils.files_io import load_jsonl
+from utils.files_io import load_jsonl, write_json_file
 import pandas as pd
 
 USERS_PATH = 'data/users.jsonl'
@@ -55,12 +55,60 @@ class Preprocessor:
         return products_within_sigma
 
     def clean_sessions(self):
-        pass
+        sessions = self.fill_missing_user_id(self.sessions)
+        sessions = self.drop_sessions_with_empty_user_id(sessions)
+        sessions = self.drop_sessions_with_empty_product_id(sessions)
+        sessions = self.drop_buy_sessions_without_purchase_id(sessions)
+        write_json_file('temp', sessions)
+
+    def fill_missing_user_id(self, sessions):
+        for i, session in enumerate(sessions):
+            if session['user_id'] is None:
+                if self.get_prev_not_null_user_id(sessions, i) == self.get_next_not_null_user_id(sessions, i):
+                    session['user_id'] = self.get_next_not_null_user_id(sessions, i)
+                elif self.get_prev_not_null_session_id(sessions, i) == session['session_id']:
+                    session['user_id'] = self.get_prev_not_null_user_id(sessions, i)
+                elif self.get_next_not_null_session_id(sessions, i) == session['session_id']:
+                    session['user_id'] = self.get_next_not_null_user_id(sessions, i)
+        return sessions
+
+    def drop_sessions_with_empty_user_id(self, sessions):
+        return [session for session in sessions if session['user_id'] is not None]
+
+    def drop_sessions_with_empty_product_id(self, sessions):
+        return [session for session in sessions if session['product_id'] is not None]
+
+    def drop_buy_sessions_without_purchase_id(self, sessions):
+        return [session for session in sessions if not
+        (session['event_type'] == 'BUY_PRODUCT' and session['purchase_id'] is None)]
+
+    def drop_sessions_without_timestamp(self, sessions):
+        return [session for session in sessions if session['timestamp'] is not None]
+
+    def get_prev_not_null_user_id(self, sessions, i):
+        while i >= 0 and sessions[i]['user_id'] is None:
+            i -= 1
+        return sessions[i]['user_id']
+
+    def get_next_not_null_user_id(self, sessions, i):
+        while i < len(sessions) and sessions[i]['user_id'] is None:
+            i += 1
+        return sessions[i]['user_id']
+
+    def get_prev_not_null_session_id(self, sessions, i):
+        while i >= 0 and sessions[i]['user_id'] is None:
+            i -= 1
+        return sessions[i]['session_id']
+
+    def get_next_not_null_session_id(self, sessions, i):
+        while i < len(sessions) and sessions[i]['user_id'] is None:
+            i += 1
+        return sessions[i]['session_id']
 
 
 def main():
     p = Preprocessor()
-    p.clean_products()
+    p.clean_sessions()
 
 
 if __name__ == '__main__':
