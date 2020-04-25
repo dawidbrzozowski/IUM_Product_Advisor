4bc9d1586666822a6d1ef3acba9905b98af20894
robertostoja-lniski <robert.lniski@gmail.com>
2020-04-25 19:04:03 +0200

    Preprocessor refactoring

--- preprocessing/preprocessor.py
+++ preprocessing/preprocessor.py
@@ -1,78 +1,82 @@
 from utils.files_io import load_jsonl, write_json_file
 import pandas as pd
 
-DEFAULT_USERS_PATH = 'data/users.jsonl'
-DEFAULT_SESSIONS_PATH = 'data/sessions.jsonl'
-DEFAULT_PRODUCTS_PATH = 'data/products.jsonl'
-
 SAVE_DIR = 'data/'
 
+DEFAULT_USERS_PATH = SAVE_DIR + 'users.jsonl'
+DEFAULT_SESSIONS_PATH = SAVE_DIR + 'sessions.jsonl'
+DEFAULT_PRODUCTS_PATH = SAVE_DIR + 'products.jsonl'
+
 USER_UNNECESSARY_ATTRIBUTES = ['street', 'name']
+SESSION_ATTRIBUTES_THAT_CANNOT_HAVE_NULL = ['user_id', 'product_id']
 
 MIN_POSSIBLE_VALUE = 0
 MAX_POSSIBLE_VALUE = 100000
 
 N_SIGMA = 3
 
-
 class Preprocessor:
 
-    def clean_users(self, users, unnecessary_attributes: list):
-        return [self._get_user_without_unnecessary_data(user, unnecessary_attributes) for user in users]
+    def clear_data(self, users, sessions, products):
+        return self._clear_users(users), self._clear_sessions(sessions), self._clear_products(products)
 
-    def _get_user_without_unnecessary_data(self, user: dict, unnecessary_attributes: list):
-        for attribute in unnecessary_attributes:
-            user.pop(attribute)
-        return user
+    def _clear_users(self, users):
+        for user in users:
+            for attribute in USER_UNNECESSARY_ATTRIBUTES:
+                user.pop(attribute)
+        return users
 
-    def clean_products(self, products):
-        products = self._get_products_with_possible_price(products)
+    def _clear_products(self, products):
+        products_prefiltered = self._remove_products_with_forbidden_values(products)
+        return self._remove_products_using_n_sigma(products_prefiltered)
+
+    def _remove_products_with_forbidden_values(self, products):
+        return [product for product in products if MIN_POSSIBLE_VALUE <= product['price'] <= MAX_POSSIBLE_VALUE]
+
+    def _remove_products_using_n_sigma(self, products):
+
         products_df = pd.DataFrame(products)
-        means = products_df[['category_path', 'price']].groupby('category_path').mean()
-        stds = products_df[['category_path', 'price']].groupby('category_path').std()
-        stds = stds.fillna(0)
-        products = self._get_products_within_sigma_n(products, means, stds, N_SIGMA)
-        return products
+        products_groupped_by_cathegory_path = products_df[['category_path', 'price']].groupby('category_path')
+        means = products_groupped_by_cathegory_path.mean()
+        stds = products_groupped_by_cathegory_path.std().fillna(0)
 
-    def _get_products_with_possible_price(self, products):
-        cleaned_products = []
-        for product in products:
-            if MIN_POSSIBLE_VALUE < product['price'] < MAX_POSSIBLE_VALUE:
-                cleaned_products.append(product)
-        return cleaned_products
-
-    def _get_products_within_sigma_n(self, products, means, stds, n):
         products_within_sigma = []
         for product in products:
-            min_value = means.loc[product['category_path']]['price'] - n * stds.loc[product['category_path']]['price']
-            max_value = means.loc[product['category_path']]['price'] + n * stds.loc[product['category_path']]['price']
-            if min_value < product['price'] < max_value:
+            current_mean = means.loc[product['category_path']]['price']
+            current_n_std = N_SIGMA * stds.loc[product['category_path']]['price']
+            if  -current_n_std < product['price'] - current_mean < current_n_std:
                 products_within_sigma.append(product)
         return products_within_sigma
 
-    def clean_sessions(self, sessions):
+    def _clear_sessions(self, sessions):
         sessions = self._fill_missing_user_id_where_possible(sessions)
-        sessions = self._drop_sessions_without_user_id(sessions)
-        sessions = self._drop_sessions_without_product_id(sessions)
+        sessions = self._drop_sessions_with_none(sessions, 'user_id')
+        sessions = self._drop_sessions_with_none(sessions, 'product_id')
         sessions = self._drop_buy_sessions_without_purchase_id(sessions)
         return sessions
-
+    
     def _fill_missing_user_id_where_possible(self, sessions):
         for i, session in enumerate(sessions):
             if session['user_id'] is None:
+                # ustaw wartosc sesji na podstawie sasiadow
                 session['user_id'] = self._find_user_id_from_neighbours(sessions, i)
         return sessions
 
     def _find_user_id_from_neighbours(self, sessions, i):
+        prev_not_null_user_id = self._get_prev_not_null_user_id(sessions, i)
+        next_not_null_user_id = self._get_next_not_null_user_id(sessions, i)
+    
         # jezeli poprzedni rekord ma taki sam user_id jak nastepny, to wpisz ten user_id
-        if self._get_prev_not_null_user_id(sessions, i) == self._get_next_not_null_user_id(sessions, i):
-            return self._get_next_not_null_user_id(sessions, i)
+        if prev_not_null_user_id == next_not_null_user_id:
+            return next_not_null_user_id
+
         # jezeli id sesji poprzedniego rekordu jest takie samo, to znaczy, ze musi to byc ten sam user_id
-        if self._get_prev_not_null_session_id(sessions, i) == sessions[i]['session_id']:
-            return self._get_prev_not_null_user_id(sessions, i)
+        if  self._get_prev_not_null_session_id(sessions, i) == sessions[i]['session_id']:
+            return prev_not_null_user_id
+
         # jezeli id sesji nastepnego rekordu jest takie samo, to znaczy, ze musi byc to ten sam user_id
         if self._get_next_not_null_session_id(sessions, i) == sessions[i]['session_id']:
-            return self._get_next_not_null_user_id(sessions, i)
+            return next_not_null_user_id
         return None
 
     def _drop_sessions_without_user_id(self, sessions):
@@ -108,29 +112,23 @@ class Preprocessor:
             i += 1
         return sessions[i]['session_id']
 
-
-def read_users_sessions_products(users_path=DEFAULT_USERS_PATH, sessions_path=DEFAULT_SESSIONS_PATH,
+def read_data(users_path=DEFAULT_USERS_PATH, sessions_path=DEFAULT_SESSIONS_PATH,
                                  products_path=DEFAULT_PRODUCTS_PATH):
     users = load_jsonl(users_path)
     sessions = load_jsonl(sessions_path)
     products = load_jsonl(products_path)
     return users, sessions, products
-
-
-def save_clean_data(users, sessions, products, save_dir):
-    preprocessor = Preprocessor()
-    clean_users = preprocessor.clean_users(users, USER_UNNECESSARY_ATTRIBUTES)
-    clean_sessions = preprocessor.clean_sessions(sessions)
-    clean_products = preprocessor.clean_products(products)
-
-    write_json_file(save_dir + 'clean-users', clean_users)
-    write_json_file(save_dir + 'clean-sessions', clean_sessions)
-    write_json_file(save_dir + 'clean-products', clean_products)
-
+    
+def write_data(clean_users, clean_sessions, clean_products):
+    write_json_file(SAVE_DIR + 'clean-users', clean_users)
+    write_json_file(SAVE_DIR + 'clean-sessions', clean_sessions)
+    write_json_file(SAVE_DIR + 'clean-products', clean_products)
 
 def main():
-    users, sessions, products = read_users_sessions_products()
-    save_clean_data(users, sessions, products, SAVE_DIR)
+    users, sessions, products = read_data()
+    preprocessor = Preprocessor()
+    clean_users, clean_sessions, clean_products = preprocessor.clear_data(users, sessions, products)
+    write_data(clean_users, clean_sessions, clean_products)
 
 
 if __name__ == '__main__':
