106efa64262e5bb584fcae9065eb1030d3cf630f
robertostoja-lniski <robert.lniski@gmail.com>
2020-06-19 19:34:46 +0200

    folder cleanup with modern preprocessor

--- preprocessing/preprocessor.py
+++ preprocessing/preprocessor.py
@@ -3,20 +3,29 @@ from preprocessing.data_cleaner import DataCleaner
 from utils.files_io import load_jsonl, write_json_file
 
 from preprocessing.merger import create_model_input
-import pandas as pd
 
-from preprocessing.timestamp_handler import TimestampHandler
-from preprocessing.user_id_filler import UserIdFiller
-
-SAVE_DIR = 'data/'
-DEFAULT_USERS_PATH = SAVE_DIR + 'users.jsonl'
-DEFAULT_SESSIONS_PATH = SAVE_DIR + 'sessions.jsonl'
-DEFAULT_PRODUCTS_PATH = SAVE_DIR + 'products.jsonl'
+DEFAULT_USERS_PATH = 'data/unprocessed/users.jsonl'
+DEFAULT_SESSIONS_PATH = 'data/unprocessed/sessions.jsonl'
+DEFAULT_PRODUCTS_PATH = 'data/unprocessed/products.jsonl'
 
 
 class Preprocessor:
+    def preprocess_data(self, raw_users, raw_sessions, raw_products):
+        pass
 
+
+class BaselinePreprocessor(Preprocessor):
     def __init__(self):
+        self.save_dir = 'data/baseline/'
+        self.data_cleaner = DataCleaner()
+
+    def preprocess_data(self, raw_users, raw_sessions, raw_products):
+        return self.data_cleaner.clear_data(raw_users, raw_sessions, raw_products)
+
+
+class NeuralNetworkPreprocessor(Preprocessor):
+    def __init__(self):
+        self.save_dir = 'data/neural_network/'
         self.data_cleaner = DataCleaner()
         self.vectorizer = Vectorizer()
 
@@ -35,21 +44,19 @@ def read_data(users_path=DEFAULT_USERS_PATH, sessions_path=DEFAULT_SESSIONS_PATH
     return users, sessions, products
 
 
-def write_data(clean_users, clean_sessions, clean_products):
-    write_json_file(SAVE_DIR + 'clean-users', clean_users)
-    write_json_file(SAVE_DIR + 'clean-sessions', clean_sessions)
-    write_json_file(SAVE_DIR + 'clean-products', clean_products)
+def write_data(save_dir, clean_users, clean_sessions, clean_products):
+    write_json_file(save_dir + 'clean-users', clean_users)
+    write_json_file(save_dir + 'clean-sessions', clean_sessions)
+    write_json_file(save_dir + 'clean-products', clean_products)
 
 
 def main():
     users, sessions, products = read_data()
-    preprocessor = Preprocessor()
+    preprocessor = NeuralNetworkPreprocessor()
     clean_users, clean_sessions, clean_products = preprocessor.preprocess_data(users, sessions, products)
-    write_data(clean_users, clean_sessions, clean_products)
+    write_data(preprocessor.save_dir, clean_users, clean_sessions, clean_products)
 
     merged_data = create_model_input(clean_users, clean_sessions, clean_products)
-
-
 
 
 if __name__ == '__main__':
