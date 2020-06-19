3b14b28a214559b971ea2000005bafee71e952a7
dawidbrzozowski <dawidbrzozowski1998@gmail.com>
2020-06-19 19:20:40 +0200

    x , y split

--- preprocessing/merger.py
+++ preprocessing/merger.py
@@ -18,19 +18,22 @@ def create_model_input(users, sessions, products):
 
 
 def split_into_x_y(merged_data):
-    print(type(merged_data))
-
-    x = []
-    y = []
+    X = [], Y = []
     for row in merged_data:
         if row['event_type'] == 'BUY_PRODUCT':
-            y.append({'session_id': row['session_id'], 'product_id': row['product_id']})
+            Y.append({'session_id': row['session_id'], 'product_id': row['product_id']})
         else:
             row.pop('event_type')
-            x.append(row)
+            X.append(row)
+    return get_matching_x_y(X, Y)
 
-    # drop session that had no buy event
-    print(len(y))
-    print(len(x))
 
-    return x, y
+def get_matching_x_y(X, Y):
+    y_sessions = set()
+    for y in Y:
+        y_sessions.add(y['session_id'])
+    X_match = []
+    for x in X:
+        if x['session_id'] in y_sessions:
+            X_match.append(x)
+    return X_match, Y
