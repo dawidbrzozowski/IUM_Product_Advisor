c9917bb87b63be8e0dcbee5e613684499c893dd5
robertostoja-lniski <robert.lniski@gmail.com>
2020-06-20 17:33:07 +0200

    Starting recommender development

--- models/neural_network.py
+++ models/neural_network.py
@@ -31,81 +31,46 @@ class NNModelTrainer:
         return self.model
 
 
-X_train = load_json('data/neural_network/X_train.json')
-y_train = load_json('data/neural_network/y_train.json')
-X_train = pd.DataFrame(X_train)
-y_train = pd.DataFrame(y_train)
-X_train.drop(columns=['session_id'], inplace=True)
-y_train.drop(columns=['session_id'], inplace=True)
-print(y_train)
+class NNIO:
+    def get_prediction(self):
+        X_train = load_json('data/neural_network/X_train.json')
+        y_train = load_json('data/neural_network/y_train.json')
+        X_train = pd.DataFrame(X_train)
+        y_train = pd.DataFrame(y_train)
+        X_train.drop(columns=['session_id'], inplace=True)
+        y_train.drop(columns=['session_id'], inplace=True)
+        print(y_train)
 
-X_test = load_json('data/neural_network/X_test.json')
-X_test_to_compare = X_test.copy()
-X_test = pd.DataFrame(X_test)
-X_test.drop(columns=['session_id'], inplace=True)
+        X_test = load_json('data/neural_network/X_test.json')
+        X_test_to_compare = X_test.copy()
+        X_test = pd.DataFrame(X_test)
+        X_test.drop(columns=['session_id'], inplace=True)
 
+        model_trainer = NNModelTrainer(288, 275)
+        model_trainer.train(X_train, y_train, 100, 5)
+        prediction = model_trainer.model.predict(X_test)
+        return prediction
 
-model_trainer = NNModelTrainer(288, 275)
-model_trainer.train(X_train, y_train, 100, 5)
-prediction = model_trainer.model.predict(X_test)
-
-clean_products = load_json('data/neural_network/clean-products.json')
-
-
-def get_prod_id_from_network_column_id(network_id, products):
-    for i, prod in enumerate(products):
-        if i == network_id:
-            return prod['product_id']
-
-
-results_to_show = 8
-processed_prediction = []
-for single_pred in prediction:
-    single_dict_pred = {}
-    for i, single_pred_for_product in enumerate(single_pred):
-        product_id = get_prod_id_from_network_column_id(i, clean_products)
-        single_dict_pred[product_id] = single_pred_for_product
-
-    sorted_single_pred = sorted(single_dict_pred.items(), key=lambda x: x[1], reverse=True)
-    sorted_single_pred_top_n = sorted_single_pred[:results_to_show]
-    processed_prediction.append(sorted_single_pred_top_n)
-
-row_id = 1
-first_prod_column = 3
-last_prod_column_exclusive = len(X_test_to_compare[row_id]) - 11
-# temporary
-view = []
-print(len(X_test_to_compare[row_id]))
-for i, product_in_session in enumerate(X_test_to_compare[row_id]):
-    # print(type(X_test_to_compare[row_id]))
-    if first_prod_column <= i < last_prod_column_exclusive and \
-            X_test_to_compare[row_id][product_in_session] != 0:
-        view.append(product_in_session)
-
-print(X_test_to_compare[row_id]['session_id'])
-
-view_names = []
-for vied_product_id in view:
-    for product in clean_products:
-        if int(vied_product_id) == product['product_id']:
-            print('matched')
-            view_names.append((product['product_name'], product['category_path']))
-
-view_processed_predictions = []
-for vied_product_id in processed_prediction[row_id]:
-    # print(vied_product_id[0], vied_product_id[1], type(vied_product_id))
-    for product in clean_products:
-        if int(vied_product_id[0]) == product['product_id']:
-            print('matched')
-            view_processed_predictions.append((product['product_name'], product['category_path'], vied_product_id[1]))
-
-print('obejrzano: ')
-for item in view_names:
-    print(item)
-
-print('polecono: ')
-for item in view_processed_predictions:
-    print(item[0], '\t\t', item[1], '\t', item[2])
-
-# prediction = [get_idx_of_predictions_over_threshold(0.02, pred) for pred in prediction]
-# print(prediction)
+# row_id = 1
+#     first_prod_column = 3
+#     last_prod_column_exclusive = len(X_test_to_compare[row_id]) - 11
+#     # temporary
+#     view = []
+#     print(len(X_test_to_compare[row_id]))
+#     for i, product_in_session in enumerate(X_test_to_compare[row_id]):
+#         if first_prod_column <= i < last_prod_column_exclusive and \
+#                 X_test_to_compare[row_id][product_in_session] != 0:
+#             view.append(product_in_session)
+#
+#     print(X_test_to_compare[row_id]['session_id'])
+#
+#     view_names = []
+#     for vied_product_id in view:
+#         for product in clean_products:
+#             if int(vied_product_id) == product['product_id']:
+#                 print('matched')
+#                 view_names.append((product['product_name'], product['category_path']))
+#
+#     print('obejrzano: ')
+#     for item in view_names:
+#         print(item)
--- recommenders/rankers.py
+++ recommenders/rankers.py
@@ -1,4 +1,6 @@
 from models.collaborative import DependencyFinder
+from utils.files_io import load_json
+from models.neural_network import NNIO
 
 
 class RecommendationGenerator:
@@ -19,3 +21,55 @@ class CollaborativeRecommendationGenerator(RecommendationGenerator):
         dependencies = self.dependency_finder.dependencies
 
 
+class NNRecommendationGenerator(RecommendationGenerator):
+
+    def __init__(self, prediction, sessions=None, recommendation_len=6):
+        self.current_recommendation = None
+        self.prediction = prediction
+        self.products = load_json('data/neural_network/clean-products.json')
+        self.sessions = sessions
+        self.recommendation_len = recommendation_len
+
+    def get_generated_recommendations(self):
+        processed_prediction = self._process_nn_output()
+        return self._deserialise_nn_output(processed_prediction)
+
+    def _get_prod_id_from_column_id(self, column_id):
+        for i, prod in enumerate(self.products):
+            if i == column_id:
+                return prod['product_id']
+
+    def _process_nn_output(self):
+        processed_prediction = []
+        for single_pred in self.prediction:
+            single_dict_pred = {}
+            for i, single_pred_for_product in enumerate(single_pred):
+                product_id = self._get_prod_id_from_column_id(i)
+                single_dict_pred[product_id] = single_pred_for_product
+
+            sorted_single_pred = sorted(single_dict_pred.items(), key=lambda x: x[1], reverse=True)
+            sorted_single_pred_top_n = sorted_single_pred[:self.recommendation_len]
+            processed_prediction.append(sorted_single_pred_top_n)
+
+        return processed_prediction
+
+    def _deserialise_nn_output(self, processed_prediction, row_id=0):
+        view_processed_predictions = []
+        for vied_product_id in processed_prediction[row_id]:
+            for product in self.products:
+                if int(vied_product_id[0]) == product['product_id']:
+                    print('matched')
+                    view_processed_predictions.append(
+                        (product['product_name'], product['category_path'], vied_product_id[1]))
+
+        print('polecono: ')
+        for item in view_processed_predictions:
+            print(item[0], '\t\t', item[1], '\t', item[2])
+
+        return view_processed_predictions
+
+nn = NNIO()
+prediction = nn.get_prediction()
+recommendation_generator = NNRecommendationGenerator(prediction)
+recommendation = recommendation_generator.get_generated_recommendations()
+print(recommendation)
