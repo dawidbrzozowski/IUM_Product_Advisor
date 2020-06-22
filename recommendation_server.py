2ba202050c3324348972573e3267a3aae8c23d85
dawidbrzozowski <dawidbrzozowski1998@gmail.com>
2020-06-22 22:51:33 +0200

    web app and server prepared for baseline

--- recommendation_server.py
+++ recommendation_server.py
@@ -2,7 +2,6 @@ from flask import Flask, request, jsonify, render_template
 
 from preprocessing.web_app_preprocessing import SessionRecommendationPreprocessor
 from recommenders.rankers import NNRecommendationGenerator
-from utils.files_io import load_json
 
 app = Flask(__name__)
 
@@ -11,7 +10,7 @@ recommendation_generator = NNRecommendationGenerator()
 products_representation = srp.create_product_representation_for_web_app()
 
 
-def get_recommendation_for_user(user_id):
+def get_recommendation_for_user_nn(user_id):
     session = srp.preprocess_user(user_id)
     recommendations = recommendation_generator.get_generated_recommendations(session)
     recommendation_texts = []
@@ -20,7 +19,7 @@ def get_recommendation_for_user(user_id):
     return recommendation_texts
 
 
-def get_recommendation_for_products(product_ids):
+def get_recommendation_for_products_nn(product_ids):
     session = srp.preprocess_products(product_ids)
     recommendations = recommendation_generator.get_generated_recommendations(session)
     recommendation_texts = []
@@ -29,45 +28,93 @@ def get_recommendation_for_products(product_ids):
     return recommendation_texts
 
 
+def get_recommendation_for_user_baseline(user_id):
+    pass
+
+
+def get_recommendation_for_products_baseline(product_ids):
+    pass
+
+
 @app.route('/')
 def home():
     return render_template('index.html', choices=products_representation)
 
 
-@app.route('/predict_user', methods=['POST'])
-def predict_user():
+@app.route('/predict_user_nn', methods=['POST'])
+def predict_user_nn():
     '''
     For rendering results on HTML GUI
     '''
     user_id = request.form.get('user_id')
-    recommendation_texts = get_recommendation_for_user(user_id)
+    recommendation_texts = get_recommendation_for_user_nn(user_id)
 
     print('[LOG INFO] prediction was made for user ', user_id)
     return render_template('index.html', choices=products_representation, prediction_text=recommendation_texts)
 
 
-@app.route('/predict_from_products', methods=['POST'])
-def predict_from_products():
+@app.route('/predict_from_products_nn', methods=['POST'])
+def predict_from_products_nn():
     '''
     For rendering results on HTML GUI
     '''
     products_repr = request.form.getlist('choices')
     product_ids = srp.web_app_product_representation_to_product_ids(products_repr)
-    recommendation_texts = get_recommendation_for_products(product_ids)
+    recommendation_texts = get_recommendation_for_products_nn(product_ids)
     print('[LOG INFO] prediction was made for product: ', products_repr)
     return render_template('index.html', choices=products_representation, prediction_text=recommendation_texts)
 
 
-@app.route('/predict_api', methods=['POST'])
-def predict_api():
+@app.route('/predict_api_nn', methods=['POST'])
+def predict_api_nn():
     '''
     For direct API calls trought request
     '''
     req = request.get_json()
     if 'user_id' in req:
-        recommendations = get_recommendation_for_user(req['user_id'])
+        recommendations = get_recommendation_for_user_nn(req['user_id'])
     elif 'session_history' in req:
-        recommendations = get_recommendation_for_products(req['session_history'])
+        recommendations = get_recommendation_for_products_nn(req['session_history'])
+    else:
+        recommendations = 'No valid data provided.'
+    return jsonify(recommendations)
+
+
+@app.route('/predict_user_baseline', methods=['POST'])
+def predict_user_baseline():
+    '''
+    For rendering results on HTML GUI
+    '''
+    user_id = request.form.get('user_id')
+    # todo wyznaczyc rekomendacje dla usera przez baseline
+
+    print('[LOG INFO] prediction was made for user ', user_id)
+    return render_template('index.html', choices=products_representation, prediction_text=recommendation_texts)
+
+
+@app.route('/predict_from_products_baseline', methods=['POST'])
+def predict_from_products_baseline():
+    '''
+    For rendering results on HTML GUI
+    '''
+    products_repr = request.form.getlist('choices')
+    product_ids = srp.web_app_product_representation_to_product_ids(products_repr)
+
+    # todo wyznaczyc rekomendacje dla historii produktow przez baseline
+    print('[LOG INFO] prediction was made for product: ', products_repr)
+    return render_template('index.html', choices=products_representation, prediction_text=recommendation_texts)
+
+
+@app.route('/predict_api_baseline', methods=['POST'])
+def predict_api_baseline():
+    '''
+    For direct API calls trought request
+    '''
+    req = request.get_json()
+    if 'user_id' in req:
+        pass # todo
+    elif 'session_history' in req:
+        pass # todo
     else:
         recommendations = 'No valid data provided.'
     return jsonify(recommendations)
