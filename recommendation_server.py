ed85f9a1496db25f91f7e266bbd78c4ba3e03d1e
dawidbrzozowski <dawidbrzozowski1998@gmail.com>
2020-06-22 22:33:04 +0200

    rest api working well

--- recommendation_server.py
+++ recommendation_server.py
@@ -11,6 +11,24 @@ recommendation_generator = NNRecommendationGenerator()
 products_representation = srp.create_product_representation_for_web_app()
 
 
+def get_recommendation_for_user(user_id):
+    session = srp.preprocess_user(user_id)
+    recommendations = recommendation_generator.get_generated_recommendations(session)
+    recommendation_texts = []
+    for recommendation in recommendations:
+        recommendation_texts.append(str(recommendation))
+    return recommendation_texts
+
+
+def get_recommendation_for_products(product_ids):
+    session = srp.preprocess_products(product_ids)
+    recommendations = recommendation_generator.get_generated_recommendations(session)
+    recommendation_texts = []
+    for recommendation in recommendations:
+        recommendation_texts.append(str(recommendation))
+    return recommendation_texts
+
+
 @app.route('/')
 def home():
     return render_template('index.html', choices=products_representation)
@@ -22,21 +40,10 @@ def predict_user():
     For rendering results on HTML GUI
     '''
     user_id = request.form.get('user_id')
-    session = srp.preprocess_user(user_id)
-    recommendations = recommendation_generator.get_generated_recommendations(session)
-    recommendation_texts = []
-    for recommendation in recommendations:
-        recommendation_texts.append(str(recommendation))
+    recommendation_texts = get_recommendation_for_user(user_id)
 
     print('[LOG INFO] prediction was made for user ', user_id)
     return render_template('index.html', choices=products_representation, prediction_text=recommendation_texts)
-
-
-@app.route('/predict_session', methods=['POST'])
-def predict_session():
-    '''
-    For rendering results on HTML GUI
-    '''
 
 
 @app.route('/predict_from_products', methods=['POST'])
@@ -46,12 +53,7 @@ def predict_from_products():
     '''
     products_repr = request.form.getlist('choices')
     product_ids = srp.web_app_product_representation_to_product_ids(products_repr)
-    session = srp.preprocess_products(product_ids)
-    recommendations = recommendation_generator.get_generated_recommendations(session)
-    recommendation_texts = []
-    for recommendation in recommendations:
-        recommendation_texts.append(str(recommendation))
-
+    recommendation_texts = get_recommendation_for_products(product_ids)
     print('[LOG INFO] prediction was made for product: ', products_repr)
     return render_template('index.html', choices=products_representation, prediction_text=recommendation_texts)
 
@@ -61,9 +63,13 @@ def predict_api():
     '''
     For direct API calls trought request
     '''
-    session = request.get_json()
-
-    recommendations = recommendation_generator.get_generated_recommendations(session)
+    req = request.get_json()
+    if 'user_id' in req:
+        recommendations = get_recommendation_for_user(req['user_id'])
+    elif 'session_history' in req:
+        recommendations = get_recommendation_for_products(req['session_history'])
+    else:
+        recommendations = 'No valid data provided.'
     return jsonify(recommendations)
 
 
